2440e7e67ef534f7b7c686a0454a45f2cb3307a1
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-09 15:24:53 -0500

    Filled logic for the GetAnimalID method.

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -243,7 +243,15 @@ namespace HumaneSociety
 
         internal static Animal GetAnimalByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Animal animal = db.Animals.Where(a => a.AnimalId == id).FirstOrDefault();
+                return animal;
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         internal static void UpdateAnimal(int animalId, Dictionary<int, string> updates)
